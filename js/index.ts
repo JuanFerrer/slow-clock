0095880a75ab91fe316ff6a111f92080e8a7e2b8
JuanFerrer <juanemilioferrer@outlook.com>
2018-02-09 11:32:11 +0000

    Bigger clock

--- js/index.ts
+++ js/index.ts
@@ -10,8 +10,8 @@ let resize = () => {
 	$(clockFaceId).attr("width", "100%");
 	$(clockFaceId).attr("height", "100%");
 
-	$(clockHandId).attr("width", "50%");
-	$(clockHandId).attr("height", "50%");
+	$(clockHandId).attr("width", "70%");
+	$(clockHandId).attr("height", "70%");
 };
 
 let getSecondsFromDate = (d: Date) => {
@@ -38,6 +38,7 @@ let updateTime = () => {
 	console.log(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 
 	setHandFromDate(date);
+	window.setTimeout(updateTime, 1000);
 };
 
 let main = () => {
